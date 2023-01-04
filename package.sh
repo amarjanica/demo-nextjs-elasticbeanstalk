177f1f62b623a382e80cbf10dcb95abca155c501
Ana Bujan <ana@eisberg-labs.com>
2023-01-04 13:19:26 +0100

    build: add package.sh

---
+++ package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -r app_name="package-demo"
+declare -r version="1.0"
+# Stops execution of the script if error
+set -e
+
+export NODE_ENV=production
+npm run build
+
+zip -r app.zip .next pages public styles *.json *.js
